b7ef0698fb9d23c66609447e123dba24cc9f6348
Christopher Norton <kicktd@gmail.com>
2017-12-03 10:25:34 -0500

    Added program icon
    Added program icon and updated version to 1.1.0
    
    Updated README.md with credit to icon author.

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("sdrtrunk_launcher")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("sdrtrunk Windows Launcher")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("sdrtrunk_launcher")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCopyright("Copyright © Chris Norton  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
--- README.md
+++ README.md
@@ -7,3 +7,7 @@ Built using Visual Studios 2017. You can either clone this project and compile f
 #### How to use:
 
 Simply place the sdrtrunk_launcher.exe file in the same folder as run_sdrtrunk_windows.bat and sdrtrunk.jar then run sdrtrunk_launcher.exe and wait for sdrtrunk to show up.
+
+#### Credits
+
+Icon made by [Freepik](http://www.freepik.com) from [www.flaticon.com](http://www.flaticon.com/)
